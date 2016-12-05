c527ba725c9fc99697a00ff8dab4262e939c7201
Andrew Jarrett <ahrjarrett@gmail.com>
2016-12-05 13:40:26 -0700

    prettify JSON output

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const request = require('request')
 const url = 'https://maps.googleapis.com/maps/api/geocode/json?address=2206%20holly%20street%20austin'
 
 request({ url, json: true }, (err, res, body) => {
-  console.log(body)
+  // 2nd arg to stringify clips properties, so passing undefined
+  // 3rd arg is how many spaces/tab
+  console.log(JSON.stringify(body, undefined, 2))
 })
 
