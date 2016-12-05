ec6a775cee4cf5a2c7b38c52970cc5b8af32d766
Andrew Jarrett <ahrjarrett@gmail.com>
2016-12-05 14:10:16 -0700

    drilled down to lat/lng

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const request = require('request')
 const url = 'https://maps.googleapis.com/maps/api/geocode/json?address=2206%20holly%20street%20austin'
 
 request({ url, json: true }, (err, res, body) => {
-  // 2nd arg to stringify clips properties, so passing undefined
-  // 3rd arg is how many spaces/tab
-  console.log(JSON.stringify(body, undefined, 2))
+  // 2nd arg clips properties, passing undefined; 3rd = # spaces/tab
+  let lat = res.body.results[0].geometry.location.lat
+  let lng = res.body.results[0].geometry.location.lng
+
+  console.log(JSON.stringify(lat, undefined, 2))
+  console.log(JSON.stringify(lng, undefined, 2))
 })
 
